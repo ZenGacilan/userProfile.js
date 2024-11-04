c0159a88db8e3f790644efa7125aacdaac89ac4f
ZenGacilan <potapetekau@gmail.com>
2024-11-04 18:41:02 +0800

    initial commit

---
+++ add.js
@@ -0,0 +1,11 @@
+// Collect user information
+const fullName = prompt("Gian Carlo Gacilan:");
+const age = parseInt(prompt("20:"));
+const favoriteNumber = parseInt(prompt("21:"));
+const favoriteColor = prompt("blue:");
+
+// Log the information to the console
+console.log("Gian Carlo M. Gacilan:", fullName);
+console.log("20:", age);
+console.log("21:", favoriteNumber);
+console.log("blue:", favoriteColor);
